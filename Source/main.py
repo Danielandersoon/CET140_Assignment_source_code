42360e11f599f78c7df0303cf645873e5203a4a4
Andy Andersoon <55076755+Danielandersoon@users.noreply.github.com>
2024-05-08 09:20:50 +0100

    Corrected file paths in main.py
    The initial commit had file paths from a previous version where I had pre-processed images and saved them to a different path.

--- Source/main.py
+++ Source/main.py
@@ -11,12 +11,10 @@ resource.setrlimit(resource.RLIMIT_AS, (24 * 1024 * 1024 * 1024, resource.RLIM_I
 
 if __name__ == '__main__':
     # Global Variables
-    _FMRI_DATA_PATH_ = './Datasets/Mri/Unnormalized'
-    _FMRI_STRIPPED_DATA_PATH_ = './Datasets/Mri/Stripped'
-    _FMRI_NORMALIZED_DATA_PATH_ = './Datasets/Mri/Normlaized'
-    _EEG_DATA_PATH_ = './Datasets/EEG/Unnormalized'
+    _FMRI_DATA_PATH_ = './Datasets/MRI'
+    _EEG_DATA_PATH_ = './Datasets/EEG'
     
 
-    #MRI_3DCNN.train_image_classifier(_FMRI_NORMALIZED_DATA_PATH_)
-    #MRI_DenseNet.train_image_classifier(_FMRI_NORMALIZED_DATA_PATH_)
+    MRI_3DCNN.train_image_classifier(_FMRI_DATA_PATH_)
+    MRI_DenseNet.train_image_classifier(_FMRI_DATA_PATH_)
     EEG_3DCNN.train_eeg_classifier(_EEG_DATA_PATH_)
